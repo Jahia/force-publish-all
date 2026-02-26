88d54a328e5b72160fc72c789b514574639a009d
Florent BOURASSE <fbourasse@jahia.com>
2026-02-26 15:29:42 +0100

    Update of Jahia URL to use as test
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-forcePublishAll.cy.ts
+++ tests/cypress/e2e/01-forcePublishAll.cy.ts
@@ -8,11 +8,7 @@ describe('Force Publish All', () => {
 
     it('shows action, opens dialog, calls mutation, and publishes to live', function() {
         cy.login()
-        cy.visit('http://localhost:8080/jahia/jcontent/digitall/en/pages/home/about?params=(sub:!f)');
-        //cy.get('#moonstone-secondaryNav_wrapper div.moonstone-selected button.moonstone-button svg.moonstone-icon').click();
-        //cy.get('#moonstone-secondaryNav_wrapper div.moonstone-selected button.moonstone-button svg.moonstone-icon').click();
-        //cy.get('#accordionContentMenu-4 li.moonstone-hover span.flexFluid').hover();
-        //cy.get('#publishMenu-352 li[data-sel-role="forcePublishAll"] span.flexFluid span').should('have.text', 'Force Publish All');
+        cy.visit('http://jahia:8080/jahia/jcontent/digitall/en/pages/home/about?params=(sub:!f)');
         cy.get('#moonstone-secondaryNav_wrapper div.moonstone-selected span.flexFluid').rightclick();
         cy.get('[data-registry-key="action:publishMenu"]').last().trigger('mouseover');
         cy.get('[data-registry-key="action:forcePublishAll"]').first().should('have.text', 'Force Publish All');
